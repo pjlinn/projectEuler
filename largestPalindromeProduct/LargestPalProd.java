d2476924886d710a4c35beab549b2c9d3f85f0e3
pjlinn <p.linnehan@gmail.com>
2013-08-18 01:22:31 -0400

    new project euler code

--- largestPalindromeProduct/LargestPalProd.java
+++ largestPalindromeProduct/LargestPalProd.java
@@ -11,6 +11,7 @@
 
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.Math.*;
 
 /*
 	The possible value range from multiplying 3 digit numbers is
@@ -20,23 +21,100 @@ import java.util.ArrayList;
 */
 
 public class LargestPalProd {
+
+	// List<Double> value = new ArrayList<>();
 	/*
 		Simple method that generates an ArrayList of the possible
 		values in the range. These are what I will check to see 
 		if they are palindromic.
 	*/
-	public static List valueRange() {
+	// public static List valueRange() {
 
-		List<Integer> values = new ArrayList();
+	// 	List<int[]> values = new ArrayList<>();
 
-		for (int i = 100000; i <= 998001; i++) {
-			values.add(i);
+	// 	counter = 0;
+
+	// 	for (int i = 998001; i >= 100000; i--) {
+
+	// 	}
+
+	// 	return values;
+	// }
+
+	// public static int largestPalindrome() {
+	// 	boolean palindrome = false;
+	// 	List<int> values = new ArrayList<>();
+
+	// 	while (!palindrome) {
+	// 		for (int i = 998001; int >= 100000; i--) {
+	// 			values.add
+	// 		}
+	// 	}
+	// }
+
+	// public static boolean largest(int num) {
+	// 	if (num % 2) {
+			
+	// 	}
+	// 	double i = Math.floor(num / Math.pow(10,Math.floor(Math.log10(num))));
+	// 	double j = Math.floor(num % Math.pow(10,Math.floor(Math.log10(num))));
+	// 	if (i == j) {
+	// 		return true;
+	// 	} 
+
+	// 	return false;
+	// }
+
+	public static boolean largestPalindrome(int num) {
+		String x = Integer.toString(num);
+
+		if (x.length() % 2 == 0) {
+			if (x.charAt(0) == x.charAt(x.length() - 1)) {
+				if (x.charAt(1) == x.charAt(x.length() - 2)) {
+					if (x.charAt(2) == x.charAt(x.length() - 3)) {
+						return true;
+					}
+				} 
+			}
+			return false;
+		} else {
+			if (x.charAt(0) == x.charAt(x.length() - 1)) {
+				if (x.charAt(1) == x.charAt(x.length() - 2)) {
+					return true;
+				}
+			}
+			return false;
 		}
-
-		return values;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(valueRange());
+		// List<Integer> possibleNum = new ArrayList<>();
+		// possibleNum = valueRange();
+
+		// System.out.println(possibleNum.get(0));
+
+		// List<Double> num = new ArrayList<>();
+		// num.add(Math.floor(654321 / Math.pow(10,Math.floor(Math.log10(654321)))));
+		// int i = num.size();
+
+		boolean isPalindrome = false;
+
+		int counter = 998001;
+
+		while (!isPalindrome) {
+			if (largestPalindrome(counter)) {
+				for (int i = 999; i >= 999/2; i--) {
+					if (counter % i == 0) {
+						if ((counter / i) < 1000) {
+							System.out.println(counter);
+							System.out.println(i);
+							isPalindrome = true;
+							break;	
+						}
+					}
+				}
+			}
+			counter -= 1;
+		}
 	}
 }
--- testCode/TestClass.java
+++ testCode/TestClass.java
@@ -3,18 +3,19 @@
 */
 
 import java.lang.Math;
+import java.util.*;
 // import static org.junit.Assert.*;
 
 public class TestClass {
 
-	public static double sumDigits(double num) {
-		if (num == 0) {
-			return 0;
-		} else {
-			// System.out.println(num / (Math.pow(10, Math.floor(Math.log10(num)))));
-			return Math.floor(num / (Math.pow(10, Math.floor(Math.log10(num))))) + sumDigits(num % (Math.pow(10, Math.floor(Math.log10(num)))));
-		}
-	}
+	// public static double sumDigits(double num) {
+	// 	if (num == 0) {
+	// 		return 0;
+	// 	} else {
+	// 		// System.out.println(num / (Math.pow(10, Math.floor(Math.log10(num)))));
+	// 		return Math.floor(num / (Math.pow(10, Math.floor(Math.log10(num))))) + sumDigits(num % (Math.pow(10, Math.floor(Math.log10(num)))));
+	// 	}
+	// }
 
 	public static void main(String[] args) {
 		// 600,851,475,143
@@ -29,12 +30,27 @@ public class TestClass {
 		// String sNum = Integer.toString(num);
 		// System.out.println(sNum[0]);
 		// System.out.println(Math.round(Math.log10(1331)));
-		System.out.println(sumDigits(0));
+		// System.out.println(sumDigits(0));
 		// System.out.println()
 		// assertEquals(0, sumDigits(0));
 		// assertEquals(8, sumDigits(1331));
 		// assertEquals(16, sumDigits(14641));
 		// assertEquals(29, sumDigits(1467821));
+
+		// List<int[]> values = new ArrayList<>();
+		// int[] anArray = new int[1];
+		// anArray[0] = 100000;
+		// values.add(anArray);
+		// System.out.println(values.get(0));
+
+		// int[] testArray = new int[1];
+		// testArray[0] = 100000;
+		// System.out.println(testArray[0]);
+
+		String i = Integer.toString(9009);
+		boolean equals = (i.charAt(0) == i.charAt(i.length() - 1));
+		System.out.println(equals);
+
 	}
 
 
