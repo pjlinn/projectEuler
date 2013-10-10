5b82b7e33f309a649bd6c7a0bc40ced2c9329fff
551294 <peterlinnehan@Macbook551294.local>
2013-10-09 22:21:34 -0400

    js even fib

--- javaScript/evenFibNumbers/script.js
+++ javaScript/evenFibNumbers/script.js
@@ -13,15 +13,35 @@ function fib (max) {
 		} else {
 			fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
 		};
-		// console.log(fibArray[i]);
 	}
 	return fibArray;
+}
+
+function isEven (fibArray) {
+	var evenFibArray = [];
+	var count = 0;
+	for (var i = 0; i < fibArray.length; i++) {
+		if (fibArray[i] % 2 == 0) {
+			evenFibArray[count] = fibArray[i];
+			count += 1;
+		};
+	}
+	return evenFibArray;
 }
 
 function printArray (fibArray) {
 	for (var i = 0; i < fibArray.length; i++){
 		console.log(fibArray[i]);
-	}
+	};
 }
 
-printArray(fib(90));
+function printSum (evenFibArray) {
+	var sum = 0;
+	for (var i = evenFibArray.length - 1; i >= 0; i--) {
+		sum += evenFibArray[i];
+	};
+	return sum;
+}
+
+// console.log(printSum(isEven(fib(4000000))));
+// alert(printSum(isEven(fib(4000000))));
