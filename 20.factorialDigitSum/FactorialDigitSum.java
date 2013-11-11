e7036982d9f345b8bca2148052e76c2a74747910
pjlinn <p.linnehan@gmail.com>
2013-11-10 20:30:17 -0500

    pe 20

---
+++ 20.factorialDigitSum/FactorialDigitSum.java
@@ -0,0 +1,49 @@
+/*
+	n! means n × (n − 1) × ... × 3 × 2 × 1
+
+	For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+	and the sum of the digits in the number 10! is 3 + 6 + 
+	2 + 8 + 8 + 0 + 0 = 27.
+
+	Find the sum of the digits in the number 100!
+*/
+
+import java.math.BigInteger;
+
+public class FactorialDigitSum {
+
+	public static int stringSum(String num){
+		int length = num.length();
+		int sum = 0;
+
+		for (int i = 0; i < length; i++) {
+			sum += num.charAt(i)-48; 
+		}
+
+		return sum;
+	}
+
+	public static BigInteger factorial(int num) {
+		
+		BigInteger factorial = new BigInteger("1");
+
+		// long factorial = 1L;
+
+		for (int i = num; i > 1; i--) {
+			BigInteger holder = new BigInteger(Integer.toString(i));
+			factorial = factorial.multiply(holder);
+		}
+		return factorial;
+	}
+
+	public static void main(String[] args) {
+		
+		String num = factorial(100).toString();
+
+		System.out.println(num);
+
+		System.out.println(stringSum(num));
+
+	}
+
+}
