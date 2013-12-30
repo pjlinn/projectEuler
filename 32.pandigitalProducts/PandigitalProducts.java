d09f466c529b1a26f104027036a0fb74f697029b
pjlinn <p.linnehan@gmail.com>
2013-12-30 11:39:52 -0500

    pe problem 32

--- 32.pandigitalProducts/PandigitalProducts.java
+++ 32.pandigitalProducts/PandigitalProducts.java
@@ -14,13 +14,17 @@
 
 	Things to note:
 	-----------------------------------
-	1. It is limited to 1-9 pandigital numbers, which all together I think
-	should equal 9! possibilities
+	1. It is limited to 1-9 pandigital numbers, which is 9! possibilities
 
 	2. You have to multipliy them, so a lot of things can be eliminated 
-	if I end up just looping through.
-		=> You only have 9 digits to work with, so the:
-		multiplier + multiplicand + product == 9 digits
+		Test: 2 digit * 3 digit = 4 digit and 1 digit * 4 digit = 4 digit
+
+	-----------------------------------
+
+	Okay I did it. Once I had all the possible permutations using the 
+	nested for loops I split each permutation into:
+		2 digit * 3 digit = 4 digit & 1 digit * 4 digit = 4 digit
+	Then I spit out the possbilities that worked.
 */
 
 import java.util.ArrayList;
@@ -28,10 +32,15 @@ import java.util.ArrayList;
 public class PandigitalProducts {
 
 	private ArrayList<Integer> numbers = new ArrayList<Integer>();
-
+	private String permutation = "";
 	/*
-		Checks to see if the number is pandigital. Just the openning
-		salvo to figuring out how to do this...
+		Constructor
+	*/
+	public PandigitalProducts () {
+		createArrayList(9);
+	}
+	/*
+		Checks to see if the number is pandigital.
 	*/
 	public static Boolean pandigitalCheck() {
 		Boolean isPandigital = true;
@@ -43,45 +52,87 @@ public class PandigitalProducts {
 		pandigital possibilities and then figure out if they can be 
 		broken into identities? 
 
-		I do not think this is a good approach however...
-
-
 		I think we should do this 9! is only 362,880
 	*/
 	public static void pandigitalNumbers() {
 
 	}
 	/*
-		A method to create a set of permutations
-
-		I want to create all possible permutations, possible approaches:
-			1. Hold the digits in an ArrayList and remove them upon
-				concatenation. Then somehow alter how I add them. Maybe I
-				add the first index and make all possible permutations with
-				that index. Then I switch the numbers in that index with the
-				next and re-run? 
-				while(numbers is not empty)
+		A method to create a set of permutations: currently is just a 
+		brute force set of for loops. Not optimal.
 	*/
 	public void getPermutations() {
-
 		int counter = 0;
+		String multiplicand, multiplier, product;
+		int iMultiplicand, iMultiplier, iProduct;
 
-		createArrayList(3);
+		for (int i = 0; i < numbers.size(); i++) {
+			for (int j = 0; j < numbers.size(); j++) {
+				if (j == i) { continue; }
+				for (int k = 0; k < numbers.size(); k++) {
+					if (k == i || k == j) { continue; }
+					for (int l = 0; l < numbers.size(); l++) {
+						if (l == i || l == j || l == k) { continue; }
+						for (int m = 0; m < numbers.size(); m++) {
+							if (m == i || m == j || m == k || m == l) { continue; }
+							for (int n = 0; n < numbers.size(); n++) {
+								if (n == i || n == j || n == k || n == l || n == m) { continue; }
+								for (int p = 0; p < numbers.size(); p++) {
+									if (p == i || p == j || p == k || p == l || p == m || p == n) { continue; }
+									for (int q = 0; q < numbers.size(); q++) {
+										if (q == i || q == j || q == k || q == l || q == m || q == n || q == p) { continue; }
+										for (int r = 0; r < numbers.size(); r++) {
+											if (r == i || r == j || r == k || r == l || r == m || r == n || r == p || r == q) { continue; }
 
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (j == i) {
-					continue;
-				}
-				for (int k = 0; k < 3; k++) {
-					if (k == i || k == j) {
-						continue;
-					}
-					System.out.println(Integer.toString(numbers.get(i)) + numbers.get(j) + numbers.get(k));
-					counter++;
+											// 2 digits * 3 digits = 4 digits
+											multiplier = Integer.toString(numbers.get(i)) + numbers.get(j);											
+											multiplicand = Integer.toString(numbers.get(k)) + numbers.get(l) + numbers.get(m);
+											product = Integer.toString(numbers.get(n)) + numbers.get(p) + + numbers.get(q) + numbers.get(r);
+
+											iMultiplier = Integer.parseInt(multiplier);
+											iMultiplicand = Integer.parseInt(multiplicand);
+											iProduct = Integer.parseInt(product);
+
+											if (iMultiplier * iMultiplicand == iProduct) {
+												System.out.println(multiplier + " * " + multiplicand + " = " + product);
+											}
+
+											// 1 digit * 4 digit = 4 digit
+											multiplier = Integer.toString(numbers.get(i)) ;											
+											multiplicand = Integer.toString(numbers.get(j)) + numbers.get(k) + numbers.get(l) + numbers.get(m);
+											product = Integer.toString(numbers.get(n)) + numbers.get(p) + + numbers.get(q) + numbers.get(r);
+
+											iMultiplier = Integer.parseInt(multiplier);
+											iMultiplicand = Integer.parseInt(multiplicand);
+											iProduct = Integer.parseInt(product);
+
+											if (iMultiplier * iMultiplicand == iProduct) {
+												System.out.println(multiplier + " * " + multiplicand + " = " + product);
+											}																	
+										}
+									}
+								}
+							}
+
+						}
+					} 
 				}
 			}
 		}
+	}
+	/*
+		Try to get permutations recursively. Currently this is nothing,
+		I was just playing around with recursion.
+	*/
+	public String recursivePermutations(int elementIndex) {
+		while (elementIndex >= 0) {
+			permutation += numbers.get(elementIndex);
+
+			elementIndex = elementIndex - 1;
+
+			recursivePermutations(elementIndex);
+		}
+		return permutation;
 	}
 	/*
 		Create an ArrayList of specified digits
@@ -100,7 +151,8 @@ public class PandigitalProducts {
 		// System.out.println(pandigitalCheck());
 
 		PandigitalProducts x = new PandigitalProducts();
-		// x.getPermutations();
+		x.getPermutations();
+		// System.out.println(x.recursivePermutations(2));
 
 
 		ArrayList<Integer> tester = new ArrayList<Integer>();
@@ -112,21 +164,21 @@ public class PandigitalProducts {
 		int b = tester.get(1);
 		int c = tester.get(2);
 		int counter = 0;
-		String permutation = "";
+		// String permutation = "";
 
-		while (counter < 6) {
-			for (int i = 0; i < tester.size(); i++) {
-				permutation = permutation + tester.get(i);
-				tester.remove(i);
-				while (tester.isEmpty() != true) {
-					permutation += tester.get(0);
-					tester.remove(0);
-				}
-				System.out.println(permutation);
-				tester.add(1); tester.add(2); tester.add(3);
-				permutation = "";
-				counter++;
-			}
-		}
+		// while (counter < 6) {
+		// 	for (int i = 0; i < tester.size(); i++) {
+		// 		permutation = permutation + tester.get(i);
+		// 		tester.remove(i);
+		// 		while (tester.isEmpty() != true) {
+		// 			permutation += tester.get(0);
+		// 			tester.remove(0);
+		// 		}
+		// 		System.out.println(permutation);
+		// 		tester.add(1); tester.add(2); tester.add(3);
+		// 		permutation = "";
+		// 		counter++;
+		// 	}
+		// }
 	}
 }
