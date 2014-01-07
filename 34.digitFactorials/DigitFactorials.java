fd17e16a6b13bdfff4a224d848cd051733d6b6b1
551294 <peterlinnehan@Macbook551294.local>
2014-01-06 21:14:55 -0500

    pe 34 update

--- 34.digitFactorials/DigitFactorials.java
+++ 34.digitFactorials/DigitFactorials.java
@@ -5,6 +5,14 @@
 	factorial of their digits.
 
 	Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+	----------
+
+	I didn't know the upper bound, so I just kept randomly 
+	increasing it until I felt like I'd reached the limit.
+
+	I could probably figure it out better using the values of
+	each factorial, but this worked fine.
 */
 
 import java.util.ArrayList;
@@ -13,31 +21,26 @@ public class DigitFactorials {
 	/*
 		Returns true if int passed is a digit factorial. False
 		otherwise.
+
+		Had to use doubles all over the place because the 
+		Math.pow() kept throwing errors when I tried to use
+		ints.
 	*/
 	public static boolean digitFactorialTest(double num) {
-		// ArrayList<Integer> digits = new ArrayList<Integer>();
 
 		double result = 0; // Sum of factorials
 		double length = lengthTest(num) - 1.; // Starting length
 		double divisor; // Starting divisor
-		double remainder; // Digit left over, which is added to the sum
-		double digit;
-		double holder = num;
+		double remainder; // Number used in the next interation
+		double digit; // This is the individual digit we take the factorial of
+		double holder = num; // Used to make the comparison
 
 		while(length >= 0) { 
-			divisor = Math.pow(10.0, length); 
-			// System.out.println("Divisor: " + divisor);
-
-			remainder = num % divisor;
-			// System.out.println("remainder: " + remainder);
-			digit = Math.floor(num / divisor);
-			// System.out.println("Digit: " + digit);
-			num = remainder;
-			// System.out.println("Num: " + num);
-
-			result = result + facatorial(digit);
-			// System.out.println("result: " + result);
-
+			divisor 	= Math.pow(10.0, length); 
+			remainder 	= num % divisor;
+			digit 		= Math.floor(num / divisor);
+			num 		= remainder;
+			result 		= result + facatorial(digit);
 			length--;
 		}
 		
@@ -85,7 +88,7 @@ public class DigitFactorials {
 		Main method
 	*/
 	public static void main(String[] args) {
-		for (double i = 10; i <= 10000000; i += 1) {
+		for (double i = 10; i <= 100000; i += 1) {
 			if (digitFactorialTest(i)) {
 				System.out.println(i + " " + digitFactorialTest(i));
 			}
