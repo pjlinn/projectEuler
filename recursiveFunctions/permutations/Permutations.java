9cda88ce8b1d15972bbffc846b159893e01de45c
551294 <peterlinnehan@Macbook551294.local>
2014-01-08 01:07:13 -0500

    permutation update

--- recursiveFunctions/permutations/Permutations.java
+++ recursiveFunctions/permutations/Permutations.java
@@ -8,26 +8,53 @@ public class Permutations {
 	/*
 		Permutation method
 	*/
-	static public ArrayList<Integer> permutationGenerator(String num) {
+	static public ArrayList<Integer> permutationGenerator(int num) {
 		ArrayList<Integer> permutations = new ArrayList<Integer>();
 		String holder1, holder2;
-		int length = num.length();
+		// int length = num.length();
 
-		if (length == 1) {
-			permutations.add(num);
-			return permutations;
-		}
+		// if (length == 1) {
+		// 	permutations.add(num);
+		// 	return permutations;
+		// }
 
-		while(permutation.equal(num)) {
-			holder1 = num.substring(length);
-			holder2 = num.substring(length - 1, length);
-			num
-		}
+		// while(!permutation.equal(num)) {
+		// 	holder1 = num.substring(length);
+		// 	holder2 = num.substring(length - 1, length);
+		// 	num
+		// }
 
 		return permutations;
 	}
-
+	/*
+		Int length method
+			Takes an int as an argument
+			Returns an Arraylist with each digit of the int in its own
+				index from 0 to n
+			This Arraylist will be passed into the permutationGenerator
+				to generate all the possible permutation of this int
+	*/
+	static public ArrayList<Integer> intLength(int num) {
+		ArrayList<Integer> digits = new ArrayList<Integer>();
+		int divisor = 1;
+		int digit;
+		int newNum = num;
+		// Determines starting divisor for the passed int
+		while(num % divisor != num) {
+			divisor = divisor * 10;
+		}
+		divisor = divisor / 10;
+		// A quick loop to break off each digit and add it to the Arraylist
+		while(divisor >= 1) {
+			newNum = newNum % divisor;
+			digit = num / divisor;
+			digits.add(digit);
+			divisor = divisor / 10;
+			num = newNum;
+		}
+		return digits;
+	}
 	public static void main(String[] args) {
-		
+		System.out.println(intLength(10002));
 	}
 }
