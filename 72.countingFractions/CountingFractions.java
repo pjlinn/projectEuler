08312d60aced1fcff7a5d39af1b3b75b6a657134
Peter Linnehan <p.linnehan@gmail.com>
2015-12-23 16:33:36 -0500

    pe problem 72 start

---
+++ 72.countingFractions/CountingFractions.java
@@ -0,0 +1,44 @@
+/*
+	Consider the fraction, n/d, where n and d are 
+	positive integers. If n<d and HCF(n,d)=1, it is called 
+	a reduced proper fraction.
+
+	If we list the set of reduced proper fractions for 
+	d ≤ 8 in ascending order of size, we get:
+
+	1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 
+	1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+	It can be seen that there are 21 elements in this set.
+
+	How many elements would be contained in the set of 
+	reduced proper fractions for d ≤ 1,000,000?
+*/
+
+import java.util.HashSet;
+
+public class CountingFractions {
+	public static void main(String[] args) {
+		
+		HashSet<Double> values = new HashSet<Double>();
+
+		Double limit = 8.;
+		Double count = limit - 1.;
+
+		for (limit = 8.; limit < 1001; limit++) {
+			for (Double numerator = 2.; numerator < limit; numerator++) {
+				for (Double denominator = limit; denominator > numerator; denominator--) {
+					if (denominator % numerator != 0.) {
+						Double result = numerator / denominator;
+						values.add(result);
+						// System.out.println(result);
+						// count++;
+						// System.out.println(numerator + "/" + denominator);
+					}
+				}
+			}
+			System.out.println(values.size() + count);
+			count = limit;
+		}
+	}
+}
