9222255716d2f4f1e5d1fec7c651cec4837cf72e
Peter Linnehan <p.linnehan@gmail.com>
2015-12-31 15:39:05 -0500

    faster implementation for 74

--- 74.digitFactorialChains/digitFactorialChains.py
+++ 74.digitFactorialChains/digitFactorialChains.py
@@ -1,14 +1,20 @@
-# But slow brute force. Want to modify it so
-# that I'm not double counting numbers I've 
-# already calculated. 
+''' 
+Runs under a minute. Checks to see if I've already found
+the chain for each calculated if number. If I have, I add
+it to what I've found so far. If I haven't, I continue and
+add once I have finished.
+'''
 
 import math
+import time
 
 # Doesn't work for numbers like 10, 100, 1000, etc.
 # Shouldn't matter though
 def factorialLink(number):
 	
-	divisor = 1000000000 # Guess on appropriate size
+	# Guess on appropriate size. I could try and
+	# parse the number so it would be length independent
+	divisor = 1000000000 
 
 	while number / divisor == 0:
 		divisor = divisor / 10		
@@ -21,27 +27,47 @@ def factorialLink(number):
 		number = number % divisor
 		divisor = divisor / 10
 	return result
+# ========================
 
-unique = True
-chainList = []
-count = 0
+startTime = time.time()
 
-# print factorialLink(145)
+unique 			= True
+count = totalLength = holder = 0
+chainList 		= []
+existingDict 	= {}
 
-for x in xrange(1,1000000):
-	chainList.append(x)
+
+for x in xrange(1,10000):
+	holder = x
+	chainList.append(x) # Add number to chain
+
+	# While the chain has not repeated
 	while unique:
+		# Next value in the chain
 		nextLink = factorialLink(x)
-		# print nextLink
-		if nextLink in chainList:
+
+		# If we know the length of the nextLink already
+		if nextLink in existingDict:
+			totalLength = len(chainList) + existingDict[nextLink]
+
+			existingDict[holder] = totalLength
 			unique = False
-			if len(chainList) == 60:
+
+			if totalLength == 60:
 				count = count + 1
 		else:
-			chainList.append(nextLink)
-			x = nextLink
+			if nextLink in chainList:
+				existingDict[holder] = len(chainList)
+				unique = False
+				if len(chainList) == 60:
+					count = count + 1
+			else:
+				chainList.append(nextLink)
+				x = nextLink
 
 	unique = True
 	chainList = []
 
+endTime = time.time()
 print count
+print endTime - startTime
