1fa6a286a5a58b8d943106faf935eab1851804d2
Peter Linnehan <p.linnehan@gmail.com>
2018-12-24 11:51:28 -0500

    Starting problem 76

---
+++ 76.countingSummations/CountingSummations.java
@@ -0,0 +1,25 @@
+/**
+ * It is possible to write five as a sum in exactly six different ways:
+ * 4 + 1
+ * 3 + 2
+ * 3 + 1 + 1
+ * 2 + 2 + 1
+ * 2 + 1 + 1 + 1
+ * 1 + 1 + 1 + 1 + 1
+ * 
+ * How many different ways can one hundred be written
+ * as a sum of at least two positive integers?
+ * -----------------
+ * My first thought is to create a map with the integers from 1 to n as the
+ * key, and the value is the number of ways to write the sum using positive
+ * integers. I should be able to use the previous values to build up new values.
+ * 
+ * If I knew the combinations for n-1, it might be easier to add the rest
+ * for n.
+ */
+
+public class CountingSummations {
+    public static void main(String[] args) {
+        
+    }
+}
