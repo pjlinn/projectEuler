a365a97833abf0b5e68573eca8a15760befa5b72
Peter Linnehan <p.linnehan@gmail.com>
2018-12-30 18:49:18 -0500

    Add clarifying comments

--- 76.countingSummations/CountingSummations.java
+++ 76.countingSummations/CountingSummations.java
@@ -32,9 +32,11 @@ public class CountingSummations {
     static Map<Integer, Map<Integer, Integer>> summations = new HashMap<>();
 
     public static Map<Integer, Integer> buildMap(Integer n, Integer i, Map<Integer, Integer> sumMap) {
+        // i increases by one each call, n stays current. I.e. n = 100, i = 1; n = 100, i = 2;
         if (n == i) {
             return sumMap;
         } else {
+            // Create 2-index arrayList with n - i and n - (n - i). The sum of the two indicies will always be N
             List<Integer> aL = new ArrayList<>();
 
             Integer x = n - i;
@@ -44,6 +46,14 @@ public class CountingSummations {
 
             Integer startingDigit = new Integer(aL.get(0));
 
+            /**
+             * When the second digit is less than the first, you can add
+             * all of its partitions. E.g. x = 97, y = 3 means all partitions of
+             * 3 work with 97 as the base number.
+             *
+             * -----
+             * Check to make sure the key is there, otherwise add it and this first partition
+             */
             if (y <= x) {
                 if (sumMap.containsKey(startingDigit)) {
                     Integer sum = sumMap.get(startingDigit);
@@ -53,7 +63,19 @@ public class CountingSummations {
                 }
             }
 
+            /**
+             * Get the map of base numbers and their partitions for y value.
+             */
             Map<Integer, Integer> partitionSums = summations.get(aL.get(1));
+            /**
+             * Loop through the map. When the key is less than or equal to the
+             * x value, you can add all the partitions. Otherwise, skip it because
+             * it would have already been added.
+             *
+             * E.g. when it's x = 4, y = 5. Loop through the map of partitions for 5.
+             * Add values when the partition base is 4 or less to the current map.
+             * Pass the values and call this funciton again with i + 1.
+             */
             for (Map.Entry<Integer, Integer> entry: partitionSums.entrySet()) {
                 if (entry.getKey() <= startingDigit) {
                     if (sumMap.containsKey(startingDigit)) {
@@ -72,15 +94,27 @@ public class CountingSummations {
 
     public static void main(String[] args) {
         summations.put(1, new HashMap<>());
-        Integer test = 100;
+        Integer end = 100;
 
-        for (Integer j = 2; j <= test; j++) {
+        /**
+         * Build a map where the key is the base value, and the value is a map where the key
+         * is first value of the partition and the value is the number of partitions with that
+         * key as the first value.
+         */
+        for (Integer j = 2; j <= end; j++) {
             Map<Integer, Integer> sum = new HashMap<>();
             Integer n = j;
             Integer i = 1;
 
+            /**
+             * j is the base value, buildMap will build the map of partition totals using previous calculations
+             * in the summation map.
+             */
             summations.put(j, buildMap(n, i, sum));
             Long total = 0L;
+            /**
+             * Sums the totals for each base value and prints it.
+             */
             for (Map.Entry<Integer, Integer> entry: summations.get(j).entrySet()) {
                 total = total + entry.getValue();
             }
